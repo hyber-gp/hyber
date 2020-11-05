a20b9c413abf0de643664631e928b5b531ce28d7
DiogoSemedo <diogo.semedo.uc@gmail.com>
2020-11-05 19:40:53 +0000

    Issue #1, first version

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,52 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+pub struct Key{
+    key_code: i8,
+    modifiers: i8
+}
+
+pub enum Keyboard{
+    KeyPressed(Key),
+    KeyReleased(Key)
+}
+
+pub enum Mouse{
+    CursorEntered,
+    CursorLeft,
+    CursorMoved {
+        x: f32,
+        y: f32,
+    },
+    WheelScroolled{
+        value: f32
+    },
+    ButtonPressed(MouseButton),
+    ButtonReleased(MouseButton)
+}
+
+pub enum MouseButton{
+    Left,
+    Right,
+    Middle
+}
+
+
+pub enum EventType{
+    Mouse(Mouse),
+    Keyboard(Keyboard)
+}
+
+
+pub trait EventSystem{
+    fn new_event() -> EventType;
+}
+
+
+pub trait Renderer {
+    
+}
+
+
+
