021614a334e8b8f29b5d132e59ff2302d37014d8
Cláudio Gomes <cfpgcp3@gmail.com>
2020-11-07 19:31:25 +0000

    Add more initial code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+pub enum Event {
+    //TODO: Rato, teclado, window
+}
+
+impl Event {
+    // TODO: funcoes
+    fn mouse_click() {
+        unimplemented!();
+    }
+
+}
+
+pub trait Display {
+    
+}
+
+struct BoxLayout {
+    min_x: unimplemented!(),
+    max_x: unimplemented!(),
+    min_y: unimplemented!(),
+    max_y: unimplemented!()
+}
+
+struct SliverLayout {
+}
+
+pub trait Widget {
+
+}
+
+pub trait Renderer {
+    
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
