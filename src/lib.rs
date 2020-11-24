3e9ec268a5a053aac31f93e65d640301e302118c
João Santos <jocatoca3@gmail.com>
2020-11-24 19:02:50 +0000

    Issue progress based on research

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+/// Enumeration with the Render Instructions
+pub enum RenderInstruction {
+    /// Instruction to the Render that a point needs to be drawn on the next Clipping
+    /// This instruction
+	DrawPoint,
+    /// Instruction to the Render that a line needs to be drawn on the next Clipping
+	DrawLine,
+	// Draw a line relative to current position
+	DrawLineRel,
+	// Draw a line from current position to absolute position
+	DrawLineTo,
+	// Draw a path
+	DrawPath,
+	// Draw a rect
+	DrawRect,
+	// Draw a triangle
+	DrawTriangle,
+	// Draw a polygon
+	DrawPolygon,
+	// Draw an arc
+	DrawArc,
+	// Draw a complete circle
+	DrawCircle,
+	// Draw an ellipse
+	DrawEllipse,
+	// Draw text at coordinates X, Y
+	DrawTextXY,
+	// Move cursor to absolute position
+	MoveTo,
+	// Move cursor relative to current position
+	MoveRel,
+	// Clear some widget from the screen 
+	Clear,
+}
+
 pub enum Event {
     //TODO: Rato, teclado, window
 }
